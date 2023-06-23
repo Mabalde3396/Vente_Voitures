c87eaa297292afbd1a4b50c92138dbf19c640359
BALDE Mamadou alimou <101585688+Mabalde3396@users.noreply.github.com>
2023-06-23 18:33:16 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @var string chaine de caractère correspondant au titre du site
+ */
+const TITLEPAGE = "BALDE VOITURES D'OCCASIONS";
+const IMGLOGO = 'logo.png';
+
+define("DB_HOST", 'db.3wa.io');
+define("DB_NAME", 'mamadoualimoubalde_voitures');
+define("DB_USER", 'mamadoualimoubalde');
+define("DB_PASS", '1faa7d01083e851c99e484171c77fe63');
